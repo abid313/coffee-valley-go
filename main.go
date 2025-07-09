94d047c01bb8fac2bc72640adc46dc43cb75f102
abid313 <abidfajar50@gmail.com>
2025-07-09 15:12:25 +0700

    edit port

--- main.go
+++ main.go
@@ -4,32 +4,39 @@ import (
 	controller "WebsiteDataOn/controllers"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// Auth Login dan Logout
+	// Ambil port dari env (Railway support ini)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // fallback untuk lokal
+	}
+
+	// Routing
 	http.HandleFunc("/", controller.Index)
 	http.HandleFunc("/login", controller.Login)
 	http.HandleFunc("/register", controller.Register)
 	http.HandleFunc("/logout", controller.Logout)
 
-	// catalog
 	http.HandleFunc("/catalog", controller.IndexCatalog)
 
-	// Distributors
 	http.HandleFunc("/distributors", controller.IndexDistributor)
 	http.HandleFunc("/distributors/add", controller.AddDistributor)
 	http.HandleFunc("/distributors/edit", controller.EditDistributor)
 	http.HandleFunc("/distributors/delete", controller.DeleteDistributor)
 
-	// Order Status
 	http.HandleFunc("/orderstatus", controller.IndexOrderStatus)
 	http.HandleFunc("/orderstatus/add", controller.AddOrderStatus)
 	http.HandleFunc("/orderstatus/edit", controller.EditOrderStatus)
 	http.HandleFunc("/orderstatus/delete", controller.DeleteOrderStatus)
 	http.HandleFunc("/orderstatus/markdone", controller.MarkDoneOrderStatus)
 
-	fmt.Println("Server started at port http://localhost:8080")
-	fmt.Println(http.ListenAndServe(":8080", nil))
-	http.ListenAndServe(":8080", nil)
+	// Jalankan server
+	fmt.Println("Server started at port :" + port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		fmt.Println("Server error:", err)
+	}
 }
